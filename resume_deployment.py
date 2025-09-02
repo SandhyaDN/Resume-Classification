5a73af7739fd4aefc0bd3f0f4f9fe05c92c3a0fd
SandhyaDN <159745919+SandhyaDN@users.noreply.github.com>
2025-09-02 11:10:36 +0530

    resume_deployment.py

--- resume_deployment.py
+++ resume_deployment.py
@@ -40,14 +40,14 @@ pipeline = Pipeline([
     ('logreg', LogisticRegression(random_state = 42, multi_class = 'multinomial'))
 ])
 # creating new x, y for pipeline usage
-#x_pipe = df['text']
+x_pipe = df['text']
 #label_en = LabelEncoder()
-#y_pipe = label_en.fit_transform(df['label'])
-#x_train_pipe, x_test_pipe, y_train_pipe, y_test_pipe = train_test_split(x_pipe, y_pipe, test_size = 0.2,
-                                                                   # random_state = 42, stratify = y_pipe)
+y_pipe = df['label']
+x_train_pipe, x_test_pipe, y_train_pipe, y_test_pipe = train_test_split(x_pipe, y_pipe, test_size = 0.2,
+                                                                    random_state = 42, stratify = y_pipe)
 
 # Fit pipeline
-pipeline.fit(x_train, y_train)
+pipeline.fit(x_train_pipe, y_train_pipe)
 
 # Save the pipeline
 joblib.dump(pipeline, 'resume_pipeline.pkl')
